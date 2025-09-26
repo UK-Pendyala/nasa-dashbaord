595c437ba709421fb12a5a298ab5c0fecf4c96c7
Umakanth Pendyala <pendyalu@oregonstate.edu>
2025-09-25 22:42:22 -0500

    Chore: setup global app dispatcher and selector hooks

--- src/app/store.ts
+++ src/app/store.ts
@@ -7,4 +7,5 @@ export const store = configureStore({
   middleware: (getDefault) => getDefault().concat(neosApi.middleware),
 });
 
+export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
